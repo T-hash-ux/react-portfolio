567766510ffbc46a867df0aad33d5c5f0b16ac02
Tyrrance <tyrrancem37@gmail.com>
2023-05-09 17:26:11 -0400

    This code is purposed to define  Footer that represents the footer section of the web page, which includes icons with links.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,3 +1,5 @@
+// The overall purpose of this code is to define a React component called Footer that represents the footer section of a web page. The footer includes information and icons for connecting with the author on GitHub and LinkedIn.
+
 import React from 'react';
 import Github from '../../images/github.png';
 import LinkedIn from '../../images/linked.png';

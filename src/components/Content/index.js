0efc42e1244e75ef4a206f376c31f88ed9355ebb
Tyrrance <tyrrancem37@gmail.com>
2023-05-09 17:12:43 -0400

    The overall purpose of this code is to define a React component called Content that renders diffent content pages.

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,9 +1,14 @@
+// The overall purpose of this code is to define a React component called Content that renders different content pages based on the value of the currentCategory prop.
+
+// The code imports the necessary dependencies: React, and four components (Contact, About, Work, Resume) from their respective files.
+
 import React from 'react';
 import Contact from '../Contact';
 import About from '../About';
 import Work from '../Work';
 import Resume from '../Resume';
 
+// The Content function component is defined, which takes currentCategory as a prop.
 
 function Content({ currentCategory }) {
 

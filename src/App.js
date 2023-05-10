b350c7ee8ce74899acaabde9193493c6de85aaee
Tyrrance <tyrrancem37@gmail.com>
2023-05-10 16:17:36 -0400

    Test

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
                 <Content currentCategory={currentCategory} />
             </main>
             <Footer/>
-            <p></p>
+        
         </div>
     );
 }
